a05c728a6952a79a1c72c99aad4144c974d42fa6
Rahul Kashyap <rahulkashyap1506@gmail.com>
2018-09-02 22:04:08 +0530

    Update

--- test.py
+++ test.py
@@ -47,7 +47,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("/home/ron/Git Repositories/HackWithInfy14/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL(getcwd()+"/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
